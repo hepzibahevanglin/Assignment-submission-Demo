8bfe997a9b9ab2f2cf81e34da6e4342f9ca8d35b
hepzibahevanglin <94317313+hepzibahevanglin@users.noreply.github.com>
2021-11-17 00:16:44 -0900

    Day 1 java

---
+++ House.java
@@ -0,0 +1,13 @@
+public class House {
+    int housenum;
+    String housename;
+    public static void main(String []args){
+        House ob = new House();
+        ob.housenum = 8147;
+        ob.housename="hepzi";
+        System.out.println(ob.housenum);
+        System.out.println(ob.housename);
+        // System.out.println("new house name-"+ ob.housename, "house num-"+ ob.housenum);
+    }
+
+}
